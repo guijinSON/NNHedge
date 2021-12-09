c04ce9c69adf20c568c6a2398061482489dcbb2b
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2021-12-09 17:36:52 +0900

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,26 @@
+@torch.no_grad()
+def plot_net_delta(model,train=False,epoch=None,PATH=None,MODEL_NAME=None):
+    model.eval()
+    input =  np.arange(0.0,1.0,0.005).reshape(-1,1)
+    output = model(torch.from_numpy(input).float()).detach().numpy() 
+    bs_delta = np.array([delta_call_BS(s,K,T,r,sigma) for s in np.arange(90,110,0.1)])
+    if train:
+        plt.plot(input, output, color = (227/255, 27/255, 35/255), label="Neural Net Delta")
+        plt.plot(input, bs_delta, color = (0.0, 45/255, 106/255), label = "Black Scholes Delta")
+
+        plt.title(f"Comparison of Deltas at Epoch {epoch+1}")
+        plt.legend()
+        plt.xlabel("Normalized S0")
+        plt.ylabel("Call Options Delta")
+        plt.savefig(f'{PATH}/{MODEL_NAME}_{epoch+1}.png')
+        plt.clf()
+    else:
+        plt.plot(input, output, color = (227/255, 27/255, 35/255), label="Neural Net Delta")
+        plt.plot(input, bs_delta, color = (0.0, 45/255, 106/255), label = "Black Scholes Delta")
+        plt.xlabel("Normalized S0")
+        plt.ylabel("Call Options Delta")
+        plt.legend()
+    model.train()
+    
+    def count_parameters(model):
+      return sum(p.numel() for p in model.parameters() if p.requires_grad)
