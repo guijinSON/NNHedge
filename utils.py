7455c3c80d520e7017f98f53d8d851c9c3fb8d8f
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2021-12-15 17:18:22 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -36,6 +36,7 @@ def resolve_shape(vector):
 
 def plot_PnL(X, Y, epoch, PATH=None, MODEL_NAME=None):
     plt.plot(X, Y, marker=".", linestyle='none', color = (0.0, 45/255, 106/255))
+    plt.figure(figsize=(4,3))
    # plt.title(f"Calculated PnL at Epoch {epoch+1}")
    # plt.xlabel("S0")
    # plt.ylabel("Calculated PnL")
