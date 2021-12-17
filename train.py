467c5df79afd0c260fa0f6cdcb11bc001ac18a2c
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2021-12-17 11:39:00 +0900

    Create train.py

---
+++ train.py
@@ -0,0 +1,25 @@
+def single_epoch_train(model, optimizer, trainloader, loss_func, model_type:str):
+    running_loss = 0.0 
+    if model_type not in ['RNN','TCN','ATTENTION','MLP']:
+        raise ValueError('Please use an available type of model. Available Models: RNN | TCN | ATTENTION |MLP')
+
+    model.train()
+    for i, data in enumerate(trainloader):
+        span, C, Sn, Sn_1 = data
+        print(span.shape)
+        if model_type == 'RNN' or model_type =='TCN':
+            span = resolve_shape(span)
+            C    = resolve_shape(C)
+            Sn   = resolve_shape(Sn)
+            Sn_1 = resolve_shape(Sn_1)
+        print(span.shape)
+        optimizer.zero_grad()
+
+        outputs = model(span)
+
+        loss = loss_func(outputs * (Sn - Sn_1) - C + torch.max(Sn - K, torch.zeros(256, 1)), torch.zeros(256, 1))
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+
+    print('[%d] loss: %.6f' % (epoch + 1, running_loss))   
